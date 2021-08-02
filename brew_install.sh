5bdd751f137e631034ebe0901f9ee1e06302369b
Raghav Khandelwal <36785216+teraflik@users.noreply.github.com>
2021-08-02 12:40:49 +0530

    Create brew_install.sh

---
+++ brew_install.sh
@@ -0,0 +1,12 @@
+# Install brew formulae
+brew install chezmoi
+brew install gnupg
+brew install tmux
+
+# Casks
+brew install --cask maccy
+brew install --cask iterm2
+brew install --cask visual-studio-code
+brew install --cask google-chrome
+brew install --cask firefox
+brew install --cask vlc
