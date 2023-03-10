369d07f29b3e21f67a5913313aa0fd06926d828b
Raghav Khandelwal <36785216+teraflik@users.noreply.github.com>
2023-03-10 16:08:49 +0530

    Add packages to brew setup script

--- brew_install.sh
+++ brew_install.sh
@@ -1,13 +1,17 @@
 # Install brew formulae
+brew install awscli
 brew install chezmoi
+brew install kubectx
+brew install libpq
+brew install mongosh
 brew install gnupg
 brew install tmux
 
 # Casks
-brew install --cask maccy
-brew install --cask iterm2
-brew install --cask visual-studio-code
-brew install --cask google-chrome
 brew install --cask firefox
-brew install --cask vlc
+brew install --cask google-chrome
+brew install --cask iterm2
 brew install --cask keepingyouawake
+brew install --cask maccy
+brew install --cask visual-studio-code
+brew install --cask vlc
